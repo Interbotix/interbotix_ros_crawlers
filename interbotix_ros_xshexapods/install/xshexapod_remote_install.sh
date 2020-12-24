dadf9691bab92ebc362697861c04c9221c1896c3
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2020-12-23 19:17:56 -0600

    Adjust install scripts to accomodate changes in the toolbox and core repos

--- interbotix_ros_xshexapods/install/xshexapod_remote_install.sh
+++ interbotix_ros_xshexapods/install/xshexapod_remote_install.sh
@@ -25,8 +25,7 @@ if [ ! -d "$INTERBOTIX_WS/src" ]; then
   catkin_make
   cd src
   git clone https://github.com/Interbotix/interbotix_ros_crawlers.git
-  # cd interbotix_ros_crawlers
-  # git checkout $ROS_NAME
+  cd interbotix_ros_crawlers && git checkout $ROS_NAME && cd ..
   echo "source $INTERBOTIX_WS/devel/setup.bash" >> ~/.bashrc
 else
   echo "Interbotix Hexapod ROS packages already installed!"
