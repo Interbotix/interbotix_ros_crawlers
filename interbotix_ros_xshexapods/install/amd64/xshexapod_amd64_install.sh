08ef4f00dd0a71980cdcf4a1b8b4adba8c076bbe
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2021-06-03 12:32:39 -0500

    fix ros gpg key expiration issue as discussed at https://discourse.ros.org/t/ros-gpg-key-expiration-incident/20669

--- interbotix_ros_xshexapods/install/amd64/xshexapod_amd64_install.sh
+++ interbotix_ros_xshexapods/install/amd64/xshexapod_amd64_install.sh
@@ -29,13 +29,14 @@ sudo apt update && sudo apt -y upgrade
 sudo apt autoremove
 
 # Install some necessary core packages
-sudo apt -y install openssh-server
+sudo apt -y install openssh-server curl
 
 # Step 1: Install ROS
 if [ $(dpkg-query -W -f='${Status}' ros-$ROS_NAME-desktop-full 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   echo "Installing ROS..."
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
   sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
   sudo apt update
   sudo apt -y install ros-$ROS_NAME-desktop-full
   if [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
